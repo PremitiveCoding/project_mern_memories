ef828a624911e248f7f46bdce7dcab56d31dbe3a
PremitiveCoding <58826270+PremitiveCoding@users.noreply.github.com>
2022-12-31 02:52:40 +0100

    geek

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import path from 'path';
 import postRoutes from './routes/posts.js';
 
 const app = express();
